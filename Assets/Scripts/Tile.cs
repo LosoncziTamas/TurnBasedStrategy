59309eeb52ccabd8810c23441d6cbd59e2844b53
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-12-30 12:07:04 +0100

    [Battlefield] add obstacle and tile hovering

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -5,11 +5,29 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private Sprite[] _sprites;
+    [SerializeField] private float _hoverAmount;
+    [SerializeField] private LayerMask _obstacleLayer;
 
     private void Start()
     {
         var spriteCount = _sprites.Length;
         var randomSpriteIndex = Random.Range(0, spriteCount);
         _spriteRenderer.sprite = _sprites[randomSpriteIndex];
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale += Vector3.one * _hoverAmount;
+    }
+    
+    private void OnMouseExit()
+    {
+        transform.localScale -= Vector3.one * _hoverAmount;
+    }
+
+    public bool IsClear()
+    {
+        var obstacle = Physics2D.OverlapCircle(transform.position, 0.2f, _obstacleLayer);
+        return obstacle == null;
     }
 }
