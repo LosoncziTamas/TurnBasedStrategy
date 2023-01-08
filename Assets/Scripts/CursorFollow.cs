3a7c4daf912d38cd613c1a57a785b3e39d520a0c
Tamás Losonczi <losonczi.tamas91@gmail.com>
2023-01-08 16:22:28 +0100

    [GUI] add menus

--- Assets/Scripts/CursorFollow.cs
+++ Assets/Scripts/CursorFollow.cs
@@ -2,17 +2,14 @@ using UnityEngine;
 
 public class CursorFollow : MonoBehaviour
 {
-    private Camera _mainCamera;
 
     private void Start()
     {
-        _mainCamera = Camera.main;
         Cursor.visible = false;
     }
 
     private void Update()
     {
-        Vector2 cursorPos = _mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = cursorPos;
+        transform.position = Input.mousePosition;
     }
 }
